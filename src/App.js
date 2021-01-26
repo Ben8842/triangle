e0df30d9f535be651d7414b54826a2bd8f190c53
Ben8842 <z87e99b44r55a@gmail.com>
2021-01-25 20:07:31 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,8 +1,87 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Hello world</div>;
+class Building extends Component {
+  renderSquare(x, y) {
+    return (
+      <button>
+        ({x} , {y})
+      </button>
+    );
+  }
+
+  render() {
+    const elementS = [];
+    const elementZ = [];
+    const viewSize = this.props.sizeValue;
+
+    var x;
+    var y;
+    for (y = 0; y < viewSize; y++) {
+      for (x = 0; x < viewSize; x++) {
+        elementS.push(<span>{this.renderSquare(x, y)}</span>);
+      }
+      elementZ.push(
+        <div className="newLine">
+          <span>
+            {elementS.map((value, index) => {
+              return <span key={index}>{value}</span>;
+            })}
+          </span>
+        </div>
+      );
+      for (x = 0; x < viewSize; x++) {
+        elementS.pop();
+      }
+    }
+    return (
+      <div className="entireThing">
+        <div>
+          <span>
+            {elementZ.map((value, index) => {
+              return <span key={index}>{value}</span>;
+            })}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+    };
+  }
+  enterCount() {
+    console.log("hello");
+    var zvalue = document.getElementById("sizeHere").value;
+    console.log(zvalue);
+    this.setState((state) => {
+      return { count: zvalue };
+    });
+  }
+
+  render() {
+    var { count } = this.state;
+
+    const inputBox = (
+      <div>
+        <form>
+          <input type="number" id="sizeHere"></input>
+          <button type="button" onClick={() => this.enterCount()}></button>
+        </form>
+      </div>
+    );
+    return (
+      <div>
+        <div>{inputBox}</div>
+        <Building sizeValue={count} />
+      </div>
+    );
+  }
 }
 
 export default App;
